8a6a7b70113dd2bb4cea163cc9f2f10a9fe126cf
9954316 <9954316@qq.com>
2018-01-29 20:40:10 +0800

    fix #21 generate test data

---
+++ scripts/crawl_lagou_job.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class JobSpider(scrapy.Spider):
+    name='job'
+
+    start_urls=['https://www.lagou.com/']
+
+    def parse(self,response):
+        for job in response.xpath(
+                './/ul[@class="clearfix position_list_ul"]/li'):
+            yield {
+                    'name':job.css('div.position_name a::text').extract_first(),
+                    'salary':job.css('span.salary ::text').extract_first(),
+                    'company_name':job.css('div.company_name a::text').extract_first(),
+                    'experience':job.css('div.position_main_info span::text').extract_first(),
+                    'img_url':job.css('div.clearfix img::attr(src)').extract_first(),
+                    'location':job.xpath('//div[@class="industry wordCut"]/span[3]/text()').extract_first(),
+                    'degree':job.xpath('//div[@class="position_main_info wordCut"]/span[2]/text()').extract_first()
+                  # 'degree':job.css('div.position_main_info span::nth-child(2)::text').extract_first()
+                    }
+
